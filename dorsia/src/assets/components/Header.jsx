e773a088ab375665325e526e093ce36e0747bfe9
bebopkenny <kennygarcia15@csu.fullerton.edu>
2025-04-14 22:51:42 -0700

    Working on hero section

---
+++ dorsia/src/assets/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <nav className="navbar">
+        <ul>
+            <li>
+                <a href="">VENUES</a>
+            </li>
+            <li>
+                <a href="">MENUS</a>
+            </li>
+            <li>
+                <a href="">DORSIA</a>
+            </li>
+            <li>
+                <a href="">PRIVATE DINING</a>
+            </li>
+            <li>
+                <a href="">EXPERIENCES</a>
+            </li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Header
---
+++ dorsia/src/assets/components/Hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+        <img src="..." className="d-block w-100" alt="..." />
+        </div>
+        <div class="carousel-item">
+        <img src="..." class="d-block w-100" alt="..." />
+        </div>
+        <div class="carousel-item">
+        <img src="..." class="d-block w-100" alt="..." />
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default Hero
